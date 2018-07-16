695376e981b62c138c836b80a8ad7dfb908b2e94
Hiu Kwok <hiufkwok@gmail.com>
2018-07-16 18:36:51 +0800

    Deck part1

--- main.go
+++ main.go
@@ -1,34 +1,16 @@
 //Only name it with main for package would be seat on main executable.
 package main
 
-import "fmt"
-
 func main () {
-	//No need to specify type String when feeding it with string data.
-	//card := "Ace of Spades"
-	//:= only work for init, re declare dun work on this way.
-	//card = "Five of Diamonds"
-	//card := newCard()
+
 
 	//Turn card into slice
-	cards := []string {
-			"Ace of Diamonds",
-			newCard()}
+	cards := newDeck()
 	//Append
 	cards = append (cards, "Six of Spades")
 
-	//Print to String
-	//fmt.Println(cards)
-
-	//Iterate
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 
 
 }
 
-
-func newCard() string {
-	return "Five of Diamonds"
-}
