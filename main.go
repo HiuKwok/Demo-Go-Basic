f38137a39e11b2d530edd8991cf781cc92e7f2a8
Hiu Kwok <hiufkwok@gmail.com>
2018-07-16 17:10:09 +0800

    For loop approach

--- main.go
+++ main.go
@@ -3,16 +3,29 @@ package main
 
 import "fmt"
 
-
 func main () {
 	//No need to specify type String when feeding it with string data.
 	//card := "Ace of Spades"
 	//:= only work for init, re declare dun work on this way.
 	//card = "Five of Diamonds"
-	card := newCard()
+	//card := newCard()
+
+	//Turn card into slice
+	cards := []string {
+			"Ace of Diamonds",
+			newCard()}
+	//Append
+	cards = append (cards, "Six of Spades")
+
+	//Print to String
+	//fmt.Println(cards)
+
+	//Iterate
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 
 
-	fmt.Println(card)
 }
 
 
