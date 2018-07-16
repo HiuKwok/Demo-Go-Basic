4decf1789e2e015580e5ddee1987df88b1a37495
Hiu Kwok <hiufkwok@gmail.com>
2018-07-16 23:37:32 +0800

    More test case

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestNewDeck(t *testing.T) {
 
@@ -14,7 +17,24 @@ func TestNewDeck(t *testing.T) {
 	if d[0] != "Ace of Spades" {
 		t.Errorf("Expected first card of Ace of Spades %v", d[0] )
 	}
+}
 
+
+func TestSaveToDeckAndNewDeckFromFile (t *testing.T){
+
+	fname := "_decktesting"
+	os.Remove(fname)
+
+	d := newDeck()
+	d.saveToFile(fname)
+
+	loadedDeck := newDeckFromFile(fname)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove(fname)
 
 
 }
