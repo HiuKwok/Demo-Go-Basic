87e58b0e768f1abeef7fc0ec786acaf6a225ca8d
Hiu Kwok <hiufkwok@gmail.com>
2018-07-16 22:22:16 +0800

    Add test case

--- deck_test.go
+++ deck_test.go
@@ -9,4 +9,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades %v", d[0] )
+	}
+
+
+
 }
